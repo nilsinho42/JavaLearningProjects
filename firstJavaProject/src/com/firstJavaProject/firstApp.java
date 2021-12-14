ad527c91451bf8d73bd203f0482bd9d83a015138
Nilson Peres <nilsontinassi2006@hotmail.com>
2021-12-14 07:13:57 -0800

    first commit

---
+++ firstJavaProject/src/com/firstJavaProject/firstApp.java
@@ -0,0 +1,14 @@
+package com.firstJavaProject;
+
+import com.firstJavaProject.model.Cat;
+
+public class firstApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat cat = new Cat("fofo", "grey", 11);
+		System.out.println(cat);
+		
+	}
+
+}
